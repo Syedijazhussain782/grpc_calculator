953c25c6bde212d9a027371bf4618ab79283d328
Syedijazhussain782 <109347153+Syedijazhussain782@users.noreply.github.com>
2023-02-02 16:27:59 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,6 @@
+import math
+
+def square_root(x):
+    print("Input is ::: ",str(x))
+    y = math.sqrt(x)
+    return y
---
+++ server.py
@@ -0,0 +1,31 @@
+import time
+from concurrent import futures
+
+import grpc
+
+import calculator_pb2
+import calculator_pb2_grpc
+
+import calculator
+
+
+class CalculatorServicer(calculator_pb2_grpc.CalculatorServicer):
+    def SquareRoot(self, request, context):
+        response = calculator_pb2.Number()
+        response.value = calculator.square_root(request.value)
+        return response
+
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+calculator_pb2_grpc.add_CalculatorServicer_to_server(CalculatorServicer(), server)
+
+print('starting server 50051')
+server.add_insecure_port('[::]:50051')
+server.start()
+
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
